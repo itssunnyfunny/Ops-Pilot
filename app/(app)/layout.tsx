e0d5f4cb0982d9531544560310e600f9b8084f3d
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-12-12 15:11:35 +0530

    forget to commit layout change when added topbar

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "@/components/layout/sidebar";
+import { Topbar } from "@/components/layout/topbar";
 
 export default function AppLayout({
     children,
@@ -8,9 +9,12 @@ export default function AppLayout({
     return (
         <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-zinc-900 via-[#0a0a0a] to-black text-foreground">
             <Sidebar />
-            <main className="ml-64 min-h-screen">
-                {children}
-            </main>
+            <div className="lg:pl-64 flex flex-col min-h-screen">
+                <Topbar />
+                <main className="flex-1">
+                    {children}
+                </main>
+            </div>
         </div>
     );
 }
